fa7454d6617741822322d91ead2cc7f6550af5fc
DannyLenk <danylenko.14@gmail.com>
2021-12-01 14:31:07 +0200

    updated battleship.js
    added view and model objects

---
+++ Chapter 8/battleship.js
@@ -0,0 +1,33 @@
+"use strict"
+
+let view = {
+   displayMessage: function(msg) {
+      const messageArea = document.getElementById('messageArea');
+      messageArea.innerHTML = msg;
+   },
+   displayHit: function(location) {
+      let cell = document.getElementById(location);
+      cell.setAttribute('class', 'hit');
+   },
+   displayMiss: function(location) {
+      let cell = document.getElementById(location);
+      cell.setAttribute('class', 'miss')
+   }
+};
+
+let model = {
+   boardSize: 7,
+   numShips: 3,
+   shipLength: 3,
+   shipsSunk: 0,
+   ships = [{ locations: ["06", "16", "26"], hits: ["", "", ""] },
+            { locations: ["24", "34", "44"], hits: ["", "", ""] },
+            { locations: ["10", "11", "12"], hits: ["", "", ""] }],
+   fire: function(guess) {
+      for (let i = 0; i < this.numShips; i++) {
+         let ship = this.ships[i];
+         let locations = ship.locations;
+      }
+   }
+};
+
