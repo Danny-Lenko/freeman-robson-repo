ebb8dfe7eb1c54f248a5ead5093a30d9641abd75
DannyLenk <danylenko.14@gmail.com>
2021-12-02 09:55:08 +0200

    updated battleship.js
    implemented basic game logic

--- Chapter 8/battleship.js
+++ Chapter 8/battleship.js
@@ -59,17 +59,54 @@ let model = {
    }
 };
 
-model.fire('53');
+let controller = {
+   guesses: 0,
 
-model.fire('06');
-model.fire('16');
-model.fire('26');
+   processGuess: function(guess) {
+      let location = parseGuess(guess);
+      if (location) {
+         this.guesses++;
+         let hit = model.fire(location);
+         if (hit && model.shipsSunk === model.numShips) {
+            view.displayMessage("You sank all my battleships, in " + this.guesses + " guesses");
+         }
+      }
+   }
+};
 
-model.fire('34');
-model.fire('24');
-model.fire('44');
+function parseGuess(guess) {
+   let alphabet = ["A", "B", "C", "D", "E", "F", "G"];
 
-model.fire('12');
-model.fire('11');
-model.fire('10');
+   if (guess === null || guess.length !==2) {
+      alert("Oops, please enter a letter and a number on the board.");
+   } else {
+      let firstChar = guess.charAt(0);
+      let row = alphabet.indexOf(firstChar);
+      let column = guess.charAt(1);
+
+      if (isNaN(row) || isNaN(column)) {
+         alert("Oops, that isn't on the board.");
+      } else if (row < 0 || row >= model.boardSize 
+                 || column < 0 || column >= model.boardSize) {
+         alert ("Oops, that's off the board!");
+      } else {
+         return row + column;
+      }
+   }
+   return null;
+}
+
+controller.processGuess("A0");
+
+controller.processGuess("A6");
+controller.processGuess("B6");
+controller.processGuess("C6");
+
+controller.processGuess("C4");
+controller.processGuess("D4");
+controller.processGuess("E4");
+
+controller.processGuess("B0");
+controller.processGuess("B1");
+controller.processGuess("B2");
 
